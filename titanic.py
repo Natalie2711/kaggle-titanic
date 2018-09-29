7bef0d4e5dd85988de9e7b1cd1f3b2bd6d6f0b9b
nhatnm0612 <nhatnguyenminh061289@gmail.com>
2018-09-29 19:42:48 +0700

    My code

---
+++ titanic.py
@@ -0,0 +1,36 @@
+import pandas as pd
+# import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+# plt.style.use("ggplot")
+
+
+train = pd.read_csv("train.csv",
+                    header=0,
+                    index_col="PassengerId",
+                    usecols=["PassengerId",
+                             "Survived",
+                             "Pclass",
+                             "Sex"])
+train["Gender"] = [0 if _ == "female" else 1 for _ in train["Sex"]]
+train.drop(["Sex"], axis=1, inplace=True)
+
+X_train = train.loc[:, ["Gender", "Pclass"]]
+Y_train = train.Survived
+
+log = LogisticRegression()
+log.fit(X_train, Y_train)
+
+test = pd.read_csv("test.csv",
+                   header=0,
+                   usecols=["PassengerId",
+                            "Pclass",
+                            "Sex"])
+
+test["Gender"] = [0 if _ == "female" else 1 for _ in test["Sex"]]
+test.drop(["Sex"], axis=1, inplace=True)
+
+x_test = test.loc[:, ["Gender", "Pclass"]]
+y_test = log.predict(x_test)
+
+df = pd.DataFrame({"PassengerId": test.PassengerId, "Survived": y_test}).set_index("PassengerId")
+df.to_csv("submission.csv")
